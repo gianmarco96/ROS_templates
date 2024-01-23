f2ea716411c92a77b5c262c58f7bb89ab33c36a8
gianmarco96 <36643645+gianmarco96@users.noreply.github.com>
2024-01-23 13:56:07 +0000

    Create Publisher.cpp

---
+++ ROS2/Publisher.cpp
@@ -0,0 +1,18 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  auto node = rclcpp::Node::make_shared("talker_node");
+  auto marker_pub = node->create_publisher<std_msgs::msg::String>("talker", 1);
+  std_msgs::msg::String st;
+  st.data = "test";
+  while (rclcpp::ok())
+  {
+    rclcpp::Rate loop_rate(1);
+    marker_pub->publish(st);
+    loop_rate.sleep();
+  }
+}
